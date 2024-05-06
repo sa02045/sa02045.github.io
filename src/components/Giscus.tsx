c5f507d4e098ffa15e5176953fecb908a5742858
devjojo <sa02045@naver.com>
2024-05-06 20:10:00 +0900

    fix: Giscus 올바른 메타데이터

--- src/components/Giscus.tsx
+++ src/components/Giscus.tsx
@@ -7,10 +7,10 @@ export default function Giscus() {
     const scriptEl = document.createElement('script');
     scriptEl.async = true;
     scriptEl.src = 'https://giscus.app/client.js';
-    scriptEl.setAttribute('data-repo', 'sa02045/blog');
-    scriptEl.setAttribute('data-repo-id', 'R_kgDOI_vGdg');
+    scriptEl.setAttribute('data-repo', 'sa02045/sa02045.github.io');
+    scriptEl.setAttribute('data-repo-id', 'R_kgDOLcbNCA');
     scriptEl.setAttribute('data-category', 'Comments');
-    scriptEl.setAttribute('data-category-id', 'DIC_kwDOI_vGds4CfK4n');
+    scriptEl.setAttribute('data-category-id', 'DIC_kwDOLcbNCM4CfK6O');
     scriptEl.setAttribute('data-mapping', 'pathname');
     scriptEl.setAttribute('data-strict', '0');
     scriptEl.setAttribute('data-reactions-enabled', '1');
