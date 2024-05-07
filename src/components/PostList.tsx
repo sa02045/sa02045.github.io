b395c540fd39109d870cc61f9dfe16a7c01c3669
devjojo <sa02045@naver.com>
2024-05-07 22:26:01 +0900

    refactor: 타입에 변경에 따른 코드 정리 및 위치 조절

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -3,13 +3,12 @@ import { Link } from 'gatsby';
 import type { Post as PostType } from '../schema';
 
 interface Props {
-  description: string;
-  date: string;
-  title: string;
+  frontmatter: PostType['frontmatter'];
   to: string;
 }
 
-function Post({ date, title, description, to }: Props) {
+function Post({ frontmatter, to }: Props) {
+  const { title, date, description } = frontmatter;
   return (
     <Link to={to} itemProp="url">
       <article
@@ -18,9 +17,9 @@ function Post({ date, title, description, to }: Props) {
         itemType="http://schema.org/Article"
       >
         <div className="flex-col">
-          <h3 className="text-2xl group-hover:text-violet-400">{title}</h3>
-          <p className="mt-6 mb-1 text-gray-600 text-base">{description}</p>
-          <span className="text-gray-500 text-sm">{date}</span>
+          <h3 className="text-2xl group-hover:text-violet-400 text-nowrap">{title}</h3>
+          <div className="text-gray-500 text-sm mt-2">{date}</div>
+          <p className="mt-2 mb-1 text-gray-800 text-base">{description}</p>
         </div>
       </article>
     </Link>
@@ -31,11 +30,8 @@ export function PostList({ posts }: { posts: PostType[] }) {
   return (
     <>
       {posts.map(post => {
-        const title = post.frontmatter.title || post.fields.slug;
-        const description = post.frontmatter.description;
-        const date = post.frontmatter.date;
         const to = post.fields.slug;
-        return <Post key={post.fields.slug} title={title} description={description} date={date} to={to} />;
+        return <Post key={post.fields.slug} to={to} frontmatter={post.frontmatter} />;
       })}
     </>
   );
