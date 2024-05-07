824de575a82d69c2a157cf8a12ea454b49573387
devjojo <sa02045@naver.com>
2024-05-07 22:25:05 +0900

    feat: webp 타입 추가, react webp 추가

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,7 @@
 declare module '*.module.css';
 declare module '*.module.scss';
 declare module '*.png';
+declare module '*.webp';
 declare module '*.svg' {
   const content: string;
   export default content;
