c516c5bab7e893e598617cc3e67829667d544c3f
devjojo <sa02045@naver.com>
2025-01-04 19:58:57 +0900

    chore: 정리

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Devjo ',
+  author: 'DevJo',
+  headerTitle: 'Devjo',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
