0d5da10be27eac4073d22ff4b5d30792ed0371d5
devjojo <sa02045@naver.com>
2025-02-16 20:26:12 +0900

    chore: font

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -3,19 +3,25 @@ import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import { ReactNode } from 'react'
 import Header from './Header'
+import localFont from 'next/font/local'
+
+const pretendard = localFont({
+  src: '../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+})
 
 interface Props {
   children: ReactNode
 }
 
-const inter = Inter({
-  subsets: ['latin'],
-})
+const inter = Inter({ subsets: ['latin'] })
 
 const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
-      <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
+      <div className={`${pretendard.className} flex h-screen flex-col justify-between font-sans`}>
         <Header />
         <main className="mb-auto">{children}</main>
         <Footer />
