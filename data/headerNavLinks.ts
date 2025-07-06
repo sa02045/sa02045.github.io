6249468e28a8277df8e09eaf106efd1a5e32cac7
devjojo <sa02045@naver.com>
2025-07-06 20:35:30 +0900

    refactor: 한글

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: '게시글' },
   { href: '/about', title: '소개' },
-  // { href: '/books', title: 'Books' },
+  // { href: '/books', title: '책' },
 ]
 
 export default headerNavLinks
