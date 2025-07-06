3ec25b9aa77e22f0c1fd5b046addcf110f0a2248
seunghee <sa02045@naver.com>
2025-07-06 20:35:47 +0900

    Refactor/post page (#28)
    * fix: hide comment
    
    * fix: page
    
    * refactor: 한글
    
    * refactor: 한글

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/about', title: 'About' },
-  // { href: '/books', title: 'Books' },
+  { href: '/blog', title: '게시글' },
+  { href: '/about', title: '소개' },
+  // { href: '/books', title: '책' },
 ]
 
 export default headerNavLinks
