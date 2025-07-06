bab8f99328e299353f818d52601fd51c5008321b
seunghee <sa02045@naver.com>
2025-07-06 20:21:44 +0900

    feat: refactor post list (#26)

--- components/HomePostList.tsx
+++ components/HomePostList.tsx
@@ -21,29 +21,28 @@ export function HomePostList({ post }: Props) {
       <li key={slug} className="py-12">
         <article className="flex flex-col items-start gap-8 md:flex-row">
           {/* 썸네일이 왼쪽 */}
-          <div className="h-32 w-48 flex-shrink-0 overflow-hidden rounded-lg bg-gray-100">
+          <div className="h-32 w-48 flex-shrink-0 overflow-hidden rounded-2xl bg-gray-100">
             <Image
               src={`/blog/${slug}`}
               alt={title + ' 썸네일'}
-              className="h-full w-full object-cover"
+              className="h-full w-full rounded-2xl object-cover"
               loading="lazy"
               width={192}
               height={108}
+              onError={(e) => {
+                e.currentTarget.src = '/static/images/placeholder.png'
+              }}
             />
           </div>
           {/* 정보가 오른쪽 */}
           <div className="flex-1">
-            <h2 className="mb-4 text-2xl font-bold leading-tight text-gray-900">{title}</h2>
-            <p className="mb-4 text-lg leading-relaxed text-gray-600">{summary}</p>
             <div className="mb-2 flex items-center space-x-2 text-sm text-gray-500">
               <span>
                 <time dateTime={date}>{formatDate(date, siteMetadata.locale)}</time>
               </span>
-              <span>•</span>
-              {tags.map((tag) => (
-                <Tag key={tag} text={tag} />
-              ))}
             </div>
+            <h2 className="mb-4 text-2xl font-bold leading-tight text-gray-900">{title}</h2>
+            <p className="mb-4 text-lg leading-relaxed text-gray-500">{summary}</p>
           </div>
         </article>
       </li>
